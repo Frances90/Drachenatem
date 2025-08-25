0235ab517dbd3b9d4b10f6d46ade17dcd7a105f6
Shadowlein <francesschmidt@hotmail.de>
2025-08-25 08:02:42 +0200

    js erweitert Berechnung der Portionsmenge

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 var portionsmenge = 4;
+var zutatenmenge = 200;
+var berechneteMenge = zutatenmenge * portionsmenge;
 
-alert("Portionsmenge: " + portionsmenge);
+/* alert("Portionsmenge: " + portionsmenge); */
+console.log("Portionsmenge: " + portionsmenge);
+console.log("Berechnete Portionsmenge: " + berechneteMenge + "g");
