23bad268716d978a75c54f15ca2a36d593d255b3
Shadowlein <francesschmidt@hotmail.de>
2025-08-25 10:26:27 +0200

    Arrays für Zutaten, Menge und Einheiten angelegt

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var portionsmenge = prompt("Anzahl Portionen:");
+var portionsmenge = 4;
+/* var portionsmenge = prompt("Anzahl Portionen:"); */
 var zutatenmenge = 200;
 var berechneteMenge = zutatenmenge * portionsmenge;
 
@@ -11,3 +12,15 @@ if (portionsmenge <= 0){
     console.log("Berechnete Portionsmenge: " + berechneteMenge + "g");
 }
 
+var zutaten = ['Huhn', 'Currypulver', 'Kokosmilch', 'Ingwer', 'Zwiebel', 'Reis', 'Zitronensaft', 'Chili'];
+var mengen = [1, 10, 250, 2, 1, 200, 20, 0.5];
+var einheiten = ['Stk.', 'g', 'ml', 'ml', 'Stk.', 'g', 'ml', 'g'];
+
+console.log("Zutat:" + zutaten[0] + " Menge:" + mengen[0] + einheiten[0]);
+
+var gesamtZutat = null;
+
+for (let i = 0; i < zutaten.length; i++) {
+    gesamtZutat = zutaten[i] + " " + mengen[i] + einheiten[i];
+    console.log(gesamtZutat);
+}
