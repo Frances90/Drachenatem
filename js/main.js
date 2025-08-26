8aa2dff4a68277a04981268e83b244e35d821476
Shadowlein <francesschmidt@hotmail.de>
2025-08-26 12:41:15 +0200

    JSON eingebaut und die Zutaten aus der JSON Datei abgerufen

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@
 var inputfield = document.getElementById("input_portionen_anzahl");
 var einzelneMengeTabel = document.getElementsByClassName("td_menge");
 var zutatenTabel = document.getElementsByClassName("td_zutaten");
+/* JSON eingebunden */
+const jsonZutaten = document.getElementById("zutaten_json"); 
+const zutatenGruppe = jsonZutaten.textContent;
+const parsedZutaten = JSON.parse(zutatenGruppe);
 var portionsmenge = 1;
-var zutatenmenge = 200;
-var berechneteMenge = zutatenmenge * portionsmenge;
+
+/* 
+Lösung Aufgabe Drachenatem - Rezept einfügen:
 var zutaten = ['Huhn', 'Currypulver', 'Kokosmilch', 'Ingwer', 'Zwiebel', 'Reis', 'Zitronensaft', 'Chili'];
 var mengen = [1, 10, 250, 2, 1, 200, 20, 0.5];
 var einheiten = ['Stk.', 'g', 'ml', 'ml', 'Stk.', 'g', 'ml', 'g'];
 var gesamtZutat = null;
+*/
 
 var button_info = document.getElementById("icon_info");
 var popup_background = document.getElementById("popup_background");
@@ -24,12 +30,18 @@ inputfield.addEventListener("change", function(){
     }else{
         console.log("Portionsmenge: " + portionsmenge);
 
-        for(let j = 0; j < mengen.length; j++){
+        for(let j = 0; j < parsedZutaten.zutat.length; j++){
+            parsedZutaten.zutat[j].menge = parsedZutaten.zutat[j].menge * portionsmenge;
+            zutatenTabel[j].innerHTML = parsedZutaten.zutat[j].zutatenart;
+            einzelneMengeTabel[j].innerHTML = parsedZutaten.zutat[j].menge + parsedZutaten.zutat[j].einheiten;
+            console.log("Zutat: " + parsedZutaten.zutat[j].zutatenart + " - " + parsedZutaten.zutat[j].menge + parsedZutaten.zutat[j].einheiten);
+            /* 
+            Lösung Aufgabe Drachenatem - Rezept einfügen:
             mengen[j] = mengen[j] * portionsmenge;
             gesamtZutat = "Zutat: " + zutaten[j] + " - " + mengen[j] + einheiten[j];
             zutatenTabel[j].innerHTML = zutaten[j];
             einzelneMengeTabel[j].innerHTML = mengen[j] + einheiten[j];          
-            console.log(gesamtZutat);
+            console.log(gesamtZutat); */
         }
     }
 } );
